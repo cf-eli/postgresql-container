b4240f02d0519846011d7119ef03fdabfc973f96
Eli <chandlerfletcher99+github@gmail.com>
2023-11-12 06:16:30 -0500

    modify column names

--- sql/01-init-db.sql
+++ sql/01-init-db.sql
@@ -1,24 +1,58 @@
-DROP TABLE IF EXISTS artists;
-CREATE TABLE IF NOT EXISTS artists (
-    id VARCHAR(255) PRIMARY KEY, --SPOTIFY_ARTIST_URI
+DROP TABLE IF EXISTS spotify_artist;
+CREATE TABLE IF NOT EXISTS spotify_artist (
+    artist_id VARCHAR(255) PRIMARY KEY,
     artist_name VARCHAR(255) NOT NULL,
-    genres VARCHAR(255),
-    image_url VARCHAR(255) NOT NULL
-    );
-
-
-DROP TABLE IF EXISTS track;
-CREATE TABLE IF NOT EXISTS track (
-    id VARCHAR(255) PRIMARY KEY, --SPOTIFY_TRACK_URI
+    genres VARCHAR(255) [],
+    artist_images VARCHAR(255) [] NOT NULL
+);
+DROP TABLE IF EXISTS spotify_track;
+CREATE TABLE IF NOT EXISTS spotify_track (
+    track_id VARCHAR(255) PRIMARY KEY,
     track_name VARCHAR(255) NOT NULL,
-    image_url VARCHAR(255),
-    release_date TIMESTAMP,
-    release_date_precision VARCHAR(255)
-    );
-
+    track_duration_ms INT NOT NULL,
+    track_number INT NOT NULL
+);
+DROP TABLE IF EXISTS spotify_track_artists;
+CREATE TABLE IF NOT EXISTS spotify_track_artists (
+    track_id VARCHAR(255),
+    artist_id VARCHAR(255),
+    FOREIGN KEY (track_id) REFERENCES spotify_track(track_id),
+    FOREIGN KEY (artist_id) REFERENCES spotify_artist(artist_id)
+);
+DROP TABLE IF EXISTS spotify_album;
+CREATE TABLE IF NOT EXISTS spotify_album (
+    album_id VARCHAR(255) PRIMARY KEY,
+    album_name VARCHAR(255) NOT NULL,
+    album_type VARCHAR(255) NOT NULL,
+    album_release_date TIMESTAMP,
+    album_release_date_precision VARCHAR(255),
+    album_total_tracks INT NOT NULL,
+    album_images VARCHAR(255) []
+);
+DROP TABLE IF EXISTS spotify_album_tracks;
+CREATE TABLE IF NOT EXISTS spotify_album_tracks (
+    album_id VARCHAR(255),
+    track_id VARCHAR(255),
+    FOREIGN KEY (album_id) REFERENCES spotify_album(album_id),
+    FOREIGN KEY (track_id) REFERENCES spotify_track(track_id)
+);
+DROP TABLE IF EXISTS spotify_user;
+CREATE TABLE IF NOT EXISTS spotify_user (
+    username VARCHAR(255) PRIMARY KEY,
+    display_name VARCHAR(255) NOT NULL,
+    user_images VARCHAR(255)
+);
+DROP TABLE IF EXISTS spotify_config;
+CREATE TABLE IF NOT EXISTS spotify_config (
+    -- store what being left off every 30 seconds for next time
+    data_id VARCHAR(255) PRIMARY KEY,
+    -- "name of what being stored"
+    context VARCHAR(255) NOT NULL -- "data being stored"
+);
 DROP TABLE IF EXISTS spotify_history;
 CREATE TABLE IF NOT EXISTS spotify_history (
-    id VARCHAR(255) PRIMARY KEY, -- USERNAME@TRACK_TIMESTAMP
+    -- USERNAME@TRACK_TIMESTAMP
+    history_id VARCHAR(255) PRIMARY KEY,
     track_timestamp TIMESTAMP NOT NULL,
     username VARCHAR(255) NOT NULL,
     platform VARCHAR(255) NOT NULL,
@@ -26,10 +60,7 @@ CREATE TABLE IF NOT EXISTS spotify_history (
     country_code VARCHAR(255),
     ip_address VARCHAR(255),
     user_agent VARCHAR(255),
-    track_name VARCHAR(255),
-    album_artist_name VARCHAR(255),
-    album_name VARCHAR(255),
-    spotify_track_uri VARCHAR(255),
+    track_id VARCHAR(255),
     reason_start VARCHAR(255),
     reason_end VARCHAR(255),
     shuffle BOOLEAN,
@@ -37,21 +68,6 @@ CREATE TABLE IF NOT EXISTS spotify_history (
     track_offline BOOLEAN,
     offline_timestamp TIMESTAMP,
     incognito_mode BOOLEAN,
-    FOREIGN KEY (spotify_track_uri) REFERENCES track(id)
-    );
-
-
-DROP TABLE IF EXISTS track_artists;
-CREATE TABLE IF NOT EXISTS track_artists (
-    track_id VARCHAR(255),
-    artist_id VARCHAR(255),
-    feature BOOLEAN NOT NULL,
-    FOREIGN KEY (track_id) REFERENCES track(id),
-    FOREIGN KEY (artist_id) REFERENCES artists(id)
+    FOREIGN KEY (track_id) REFERENCES spotify_track(track_id),
+    FOREIGN KEY (username) REFERENCES spotify_user(username)
 );
-
-DROP TABLE IF EXISTS spotify_data;
-CREATE TABLE IF NOT EXISTS spotify_data ( -- store what being left off every 30 seconds for next time
-    id VARCHAR(255) PRIMARY KEY, -- "name of what being stored"
-    id_data VARCHAR(255) NOT NULL -- "data being stored"
-)
