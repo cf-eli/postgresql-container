b47002fffe807e09f29f350a53bf44e9dff2178e
Eli <chandlerfletcher99+github@gmail.com>
2023-12-03 02:22:05 -0500

    Finalize db

--- sql/01-init-db.sql
+++ sql/01-init-db.sql
@@ -1,3 +1,4 @@
+SET timezone = 'UTC';
 DROP TABLE IF EXISTS spotify_artist;
 CREATE TABLE IF NOT EXISTS spotify_artist (
     artist_id VARCHAR(255) PRIMARY KEY,
@@ -24,10 +25,11 @@ CREATE TABLE IF NOT EXISTS spotify_album (
     album_id VARCHAR(255) PRIMARY KEY,
     album_name VARCHAR(255) NOT NULL,
     album_type VARCHAR(255) NOT NULL,
-    album_release_date TIMESTAMP,
+    album_release_date TIMESTAMP WITH TIME ZONE,
     album_release_date_precision VARCHAR(255),
     album_total_tracks INT NOT NULL,
-    album_images VARCHAR(255) []
+    album_images VARCHAR(255) [],
+    album_artists VARCHAR(255) []
 );
 DROP TABLE IF EXISTS spotify_album_tracks;
 CREATE TABLE IF NOT EXISTS spotify_album_tracks (
@@ -40,12 +42,15 @@ DROP TABLE IF EXISTS spotify_user;
 CREATE TABLE IF NOT EXISTS spotify_user (
     username VARCHAR(255) PRIMARY KEY,
     display_name VARCHAR(255) NOT NULL,
-    user_images VARCHAR(255)
+    user_images VARCHAR(255),
+    token_info JSON
 );
 DROP TABLE IF EXISTS spotify_config;
 CREATE TABLE IF NOT EXISTS spotify_config (
     -- store what being left off every 30 seconds for next time
     data_id VARCHAR(255) PRIMARY KEY,
+    context_timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+    -- "when was this stored"
     -- "name of what being stored"
     context VARCHAR(255) NOT NULL -- "data being stored"
 );
@@ -53,7 +58,7 @@ DROP TABLE IF EXISTS spotify_history;
 CREATE TABLE IF NOT EXISTS spotify_history (
     -- USERNAME@TRACK_TIMESTAMP
     history_id VARCHAR(255) PRIMARY KEY,
-    track_timestamp TIMESTAMP NOT NULL,
+    track_timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
     username VARCHAR(255) NOT NULL,
     platform VARCHAR(255) NOT NULL,
     ms_played INT NOT NULL,
@@ -66,8 +71,18 @@ CREATE TABLE IF NOT EXISTS spotify_history (
     shuffle BOOLEAN,
     skipped BOOLEAN,
     track_offline BOOLEAN,
-    offline_timestamp TIMESTAMP,
+    offline_timestamp TIMESTAMP WITH TIME ZONE,
     incognito_mode BOOLEAN,
+    FOREIGN KEY (track_id) REFERENCES spotify_track(track_id),
+    FOREIGN KEY (username) REFERENCES spotify_user(username)
+);
+DROP TABLE IF EXISTS spotify_recently_played;
+CREATE TABLE IF NOT EXISTS spotify_recently_played (
+    -- USERNAME@TRACK_TIMESTAMP
+    recently_played_id VARCHAR(255) PRIMARY KEY,
+    played_at TIMESTAMP WITH TIME ZONE,
+    track_id VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     FOREIGN KEY (track_id) REFERENCES spotify_track(track_id),
     FOREIGN KEY (username) REFERENCES spotify_user(username)
 );
